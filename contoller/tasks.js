6c13e5e4ed3b455ee779d0da7d501bdaa9bfefc2
unknown <vikas1000dhiman@outlook.com>
2022-06-30 08:38:10 +0530

    Resolve bug

--- contoller/tasks.js
+++ contoller/tasks.js
@@ -1,9 +1,9 @@
 const Task = require('../models/task')
 const getAllTasks = async (req, res) => {
   try {
-    const task = await Task.find({})
+    const tasks = await Task.find({})
 
-    res.status(200).json({ task })
+    res.status(200).json({ tasks })
   } catch (error) {
     res.status(500).json({ msg: error })
   }
